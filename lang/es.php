bba132d68efe4586508098a59da980f2403c7a4c
Filisko <filisko@autistici.org>
2015-03-12 09:45:51 +0100

    Create es.php

---
+++ lang/es.php
@@ -0,0 +1,20 @@
+<?php
+//lang file for editor module
+//part of Pluck-cms
+
+$lang['editor']['name'] = 'Módulo editor de plantillas';
+$lang['editor']['intro'] = 'Este módulo te permite editar el archivo de la plantilla.';
+ang['editor']['author'] = 'Bas Steelooper (reescritura del módulo desde A_Bach to 4.7)';
+$lang['editor']['main'] = 'Elige para editar el archivo de la plantilla o el archivo CSS.';
+$lang['editor']['theme'] = 'Editor de temas';
+$lang['editor']['css'] = 'Editor CSS';
+$lang['editor']['info'] = 'Información PHP';
+$lang['editor']['edit_css'] = 'Edita tu archivo CSS';
+$lang['editor']['edit_theme'] = 'Edita el archivo de la plantilla';
+$lang['editor']['edit_info'] = 'Información';
+$lang['editor']['edit_css_info'] = 'Te permite editar tu archivo CSS.';
+$lang['editor']['edit_theme_info'] = 'Te permite editar el archivo de la plantilla.';
+$lang['editor']['edit_info_info'] = 'Información PHP';
+$lang['editor']['content_theme'] = 'Contenido del archivo de la plantilla';
+$lang['editor']['content_css'] = 'Contenido del archivo CSS';
+?>
